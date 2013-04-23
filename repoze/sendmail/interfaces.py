f5f917b784aa73e6bb53b6ca8297beebb26ee2cd
Tres Seaver <tseaver@palladion.com>
2013-04-23 16:46:53 -0400

    Clean out fossils.

--- repoze/sendmail/interfaces.py
+++ repoze/sendmail/interfaces.py
@@ -11,52 +11,43 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Mailer interfaces
+"""`repoze.sendmail` interfaces
 
-Email sending from Zope 3 applications works as follows:
+Sending e-mail from and applications works as follows:
 
-- A Zope 3 application locates a mail delivery utility (`IMailDelivery`) and
-  feeds a message to it. It gets back a unique message ID so it can keep
-  track of the message by subscribing to `IMailEvent` events.
+- An application locates a mail delivery utility (`IMailDelivery`).
+  Applications may also instantiate a particular delivery class directly.
 
-- The utility registers with the transaction system to make sure the
-  message is only sent when the transaction commits successfully.  (Among
-  other things this avoids duplicate messages on `ConflictErrors`.)
+- The application feeds a message to the delivery instance, geting back a
+  unique message ID. 
 
-- If the delivery utility is a `IQueuedMailDelivery`, it puts the message into
-  a queue (a Maildir mailbox in the file system). A separate process or thread
-  (`IMailQueueProcessor`) watches the queue and delivers messages
-  asynchronously. Since the queue is located in the file system, it survives
-  Zope restarts or crashes and the mail is not lost.  The queue processor
-  can implement batching to keep the server load low.
+- The deliver registers with the transaction system to make sure the
+  message is only sent when the transaction commits successfully.  Among
+  other things this avoids duplicate messages on `ConflictErrors`.
 
-- If the delivery utility is a `IDirectMailDelivery`, it delivers messages
-  synchronously during the transaction commit.  This is not a very good idea,
-  as it makes the user wait.  Note that transaction commits must not fail,
-  but that is not a problem, because mail delivery problems dispatch an
-  event instead of raising an exception.
+- A queuing delivery puts the message into a queue (a Maildir mailbox in
+  the filesystem). A separate process or thread watches the queue and
+  delivers messages asynchronously.  Since the queue is located in the
+  filesystem, it survives application restarts or crashes and the mail is not
+  lost.  The queue processor can implement batching to keep the server load low.
 
-  However, there is a problem -- sending events causes unknown code to be
-  executed during the transaction commit phase.  There should be a way to
-  start a new transaction for event processing after this one is commited.
+- A direct delivery delivers messages synchronously via SMPT during the
+  transaction commit..
 
-- An `IMailQueueProcessor` or `IDirectMailDelivery` actually delivers the
+- The queue processor and the direct delivery actually delivers the
   messages by using a mailer (`IMailer`) component that encapsulates the
-  delivery process.  There currently is only one mailer:
+  delivery process.  The package provides two mailers:
 
-    - `ISMTPMailer` sends all messages to a relay host using SMTP
+    - `SMTPMailer` sends all messages to a relay host using SMTP
 
-- If mail delivery succeeds, an `IMailSentEvent` is dispatched by the mailer.
-  If mail delivery fails, no exceptions are raised, but an `IMailErrorEvent` is
-  dispatched by the mailer.
+    - 'SendmailMailer` sends all messages using the `sendmail` command.
 """
 
 from zope.interface import Interface
 
 class IMailDelivery(Interface):
-    """A mail delivery utility allows someone to send an email to a group of
-    people."""
-
+    """Send an email to a group of people.
+    """
     def send(fromaddr, toaddrs, message):
         """Send an email message.
 
@@ -66,20 +57,16 @@ class IMailDelivery(Interface):
 
         `message` is a `Message` object from the stdlib
         `email.message` module.  If it does not contain a Message-Id
-        header, it will be generated and added automatically.
+        header, one will be generated and added automatically.
 
         Returns the message ID.
-
-        You can subscribe to `IMailEvent` events for notification about
-        problems or successful delivery.
 
         Messages are actually sent during transaction commit.
         """
 
-
 class IMailer(Interface):
-    """Mailer handles synchronous mail delivery."""
-
+    """Handles synchronous mail delivery.
+    """
     def send(fromaddr, toaddrs, message):
         """Send an email message.
 
@@ -89,10 +76,7 @@ class IMailer(Interface):
 
         `message` is a `Message` object from the stdlib
         `email.message` module.  If it does not contain a Message-Id
-        header, it will be generated and added automatically.
+        header, one will be generated and added automatically.
 
         Messages are sent immediatelly.
-
-        Dispatches an `IMailSentEvent` on successful delivery, otherwise an
-        `IMailErrorEvent`.
         """
